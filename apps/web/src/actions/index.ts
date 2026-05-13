8d5ae305e9d56783b85eb6bcfb08da8a9fdc22d3
RochTools <hubproapk@gmail.com>
2026-05-13 16:14:14 +0500

    Add isActionWithOptionalArgs function for validation

--- apps/web/src/actions/index.ts
+++ apps/web/src/actions/index.ts
@@ -1,3 +1,12 @@
 export * from "./definitions";
 export * from "./types";
 export * from "./registry";
+
+import { ACTIONS } from "./definitions";
+import type { TActionWithOptionalArgs } from "./types";
+
+const ACTION_SET = new Set<string>(Object.keys(ACTIONS));
+
+export function isActionWithOptionalArgs(value: string): value is TActionWithOptionalArgs {
+	return ACTION_SET.has(value);
+}
